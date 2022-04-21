41f0f1730bf7483b1f6eb8ebe67f2607ad1a5bf8
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-21 10:56:41 +0200

    Rename module

--- Provider/src/main/java/module-info.java
+++ Provider/src/main/java/module-info.java
@@ -2,7 +2,7 @@ import org.example.calculatorprovider.PensionerCalculator;
 import org.example.calculatorprovider.WorkingAgeCalculator;
 import org.example.taxcalculator.TaxCalculator;
 
-module Provider {
+module org.example.calculatorprovider {
     requires org.example.taxcalculator;
     provides TaxCalculator with PensionerCalculator, WorkingAgeCalculator;
 }
