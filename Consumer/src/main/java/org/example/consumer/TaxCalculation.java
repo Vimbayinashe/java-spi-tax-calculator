10e5adacbbad4453aa61970a3de8d12942274c52
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-04-25 11:26:09 +0200

    Fix - get annotated tax calculator

--- Consumer/src/main/java/org/example/consumer/TaxCalculation.java
+++ Consumer/src/main/java/org/example/consumer/TaxCalculation.java
@@ -1,9 +1,11 @@
 package org.example.consumer;
 
 import org.example.taxcalculator.TaxCalculator;
+import org.example.taxcalculator.TaxCategory;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.ServiceLoader;
 
@@ -11,9 +13,11 @@ public class TaxCalculation {
 
     public static Scanner scanner = new Scanner(System.in);
     private final int age;
+    private final String category;
 
     public TaxCalculation(int age) {
         this.age = age;
+        this.category = category(age);
     }
 
     public void start() {
@@ -33,13 +37,18 @@ public class TaxCalculation {
 
     }
 
+    private String category(double income) {
+        return age > 65 ? "pension" : "ordinary";
+    }
+
     private NetIncomeAndTax calculate(double income) {
         return age > 65 ? pensioner(income) : workingAge(income);
+        // return handleCalculation(income);
     }
 
     private double getTotalIncome() {
         double income;
-        while(true) {
+        while (true) {
             System.out.println("Please enter your total income");
             String input = scanner.nextLine().replace(",", ".");
 
@@ -59,27 +68,71 @@ public class TaxCalculation {
     }
 
     private NetIncomeAndTax pensioner(double income) {
-        TaxCalculator calculator = getTaxCalculator("Pensioner");
+        TaxCalculator calculator = getTaxCalculator("pension");
         var tax = calculator.calculateTax(income);
         var netIncome = calculator.incomeAfterTax(income);
         return new NetIncomeAndTax(netIncome, tax);
     }
 
     private NetIncomeAndTax workingAge(double income) {
-        TaxCalculator calculator = getTaxCalculator("Working");
+        TaxCalculator calculator = getTaxCalculator("ordinary");
         var tax = calculator.calculateTax(income);
         var netIncome = calculator.incomeAfterTax(income);
         return new NetIncomeAndTax(netIncome, tax);
     }
 
-    private static TaxCalculator getTaxCalculator(String ageGroup) {
+    private static TaxCalculator getTaxCalculator(String category) {
+        ServiceLoader<TaxCalculator> serviceLoader = ServiceLoader.load(TaxCalculator.class);
+        Optional<TaxCalculator> taxCalculator = Optional.empty();
+
+        for (TaxCalculator calculator : serviceLoader) {
+            var annotation = calculator.getClass().getAnnotation(TaxCategory.class);
+
+            if (annotation != null && category.equals(annotation.value()))
+                taxCalculator = Optional.of(calculator);
+        }
+
+        return taxCalculator.orElseThrow(() -> new IllegalArgumentException(category + " category not found"));
+    }
+
+    private static boolean hasCategory(String category, ServiceLoader.Provider<TaxCalculator> taxCalculator) {
+        System.out.print("has category: ");
+        System.out.println(category.equals(taxCalculator.getClass().getAnnotation(TaxCategory.class).value()));
+        return category.equals(taxCalculator.getClass().getAnnotation(TaxCategory.class).value());
+    }
+
+    private static boolean hasAnnotationValue(ServiceLoader.Provider<TaxCalculator> taxCalculator) {
+        System.out.print("has annotation value: ");
+        System.out.println(taxCalculator.getClass().getAnnotation(TaxCategory.class) != null);
+        return taxCalculator.getClass().getAnnotation(TaxCategory.class) != null;
+    }
+
+
+    private NetIncomeAndTax handleCalculation(double income) {
+        String category = age > 65 ? "pension" : "ordinary";
+
+        TaxCalculator calculator = getTaxCalculator(category);
+        var tax = calculator.calculateTax(income);
+        var netIncome = calculator.incomeAfterTax(income);
+        return new NetIncomeAndTax(netIncome, tax);
+    }
+
+
+    public static void main(String[] args) {
         ServiceLoader<TaxCalculator> serviceLoader = ServiceLoader.load(TaxCalculator.class);
 
-        var calculator = serviceLoader.stream()
-                .filter(provider -> provider.type().getSimpleName().startsWith(ageGroup))
-                .map(ServiceLoader.Provider::get)
-                .findFirst();
+        for (TaxCalculator calculator : serviceLoader) {
+            var annotation = calculator.getClass().getAnnotation(TaxCategory.class);
 
-        return calculator.orElseThrow(() -> new IllegalArgumentException(ageGroup + " age group not found"));
+            if (annotation != null) {
+                System.out.println("Annotation on class - " + annotation.value());
+
+                if ("pension".equals(annotation.value()))
+                    System.out.println(annotation.value() + " is equal to " + "pension");
+
+            }
+
+            System.out.println("------------------------------");
+        }
     }
 }
